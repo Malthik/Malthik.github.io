425bdf34b0b8fa4337f3594788814525c261e5e7
Malthik <gustavo_malthik@hotmail.com>
2023-02-10 20:01:25 -0300

    usando o HostListener para mudar o carousel de acordo com a largura da tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { environment } from './../environments/environment';
 import { DataModel } from './models/data_model';
@@ -15,12 +15,13 @@ export class AppComponent implements OnInit {
   inProgress = true;
   noData = false;
   noDataInfo = 'This page is under maintenance, please come back later.';
+  carouselValue: number = 0;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     const url = environment.apiUrls.v1 + 'data';
-
+    this.setCarouselValues(window.innerWidth);
     this.http
       .get<DataModel>(url)
       .pipe<DataModel>(
@@ -33,5 +34,23 @@ export class AppComponent implements OnInit {
         this.inProgress = false;
         this.data = res;
       });
+  }
+
+  setCarouselValues(width: number) {
+    this.carouselValue =
+      width < 400
+        ? 1
+        : width < 600
+        ? 2
+        : width < 840
+        ? 3
+        : width < 1200
+        ? 4
+        : 5;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.setCarouselValues(window.innerWidth);
   }
 }
